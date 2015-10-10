4a459fa93fd981709a2f321a1574823b25ca8a75
Dany Gilbert <thorfin.pourfendeur@gmail.com>
2015-10-10 18:47:53 -0400

    Added difficulty slider

---
+++ Assets/Scripts/MenuCtrl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class MenuCtrl : MonoBehaviour
+{
+    private Text difficultyOutput;
+    private String[] difficultyLevels = { "Tutoriel", "Facile", "Moyen", "Difficile" };
+    private Slider difficultySlider;
+
+    void Start()
+    {
+        difficultyOutput = GameObject.Find("Difficulty Output").GetComponent<Text>();
+        difficultySlider = GameObject.Find("Difficulty").GetComponent<Slider>();
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void AdjustDifficulty()
+    {
+        difficultyOutput.text = difficultyLevels[(int)difficultySlider.value];
+    }
+}
