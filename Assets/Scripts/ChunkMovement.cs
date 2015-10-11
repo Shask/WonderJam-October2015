4cbd0e8b82d36b21e344e3880bd98fddf1904226
Steven Fougeron <steven.fougeron@gmail.com>
2015-10-11 13:37:09 +0200

    Chunk and Chunk generation finished

---
+++ Assets/Scripts/ChunkMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChunkMovement : MonoBehaviour {
+
+    public float ChunkSpeed = 3.0f;
+   
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float y = ChunkSpeed * Time.deltaTime;
+        Vector3 NewPos = transform.position;
+        NewPos.y += y;
+        transform.position = NewPos;
+
+        if (transform.position.y >= 30)
+        {
+            Destroy(gameObject);
+        }
+    }
+   
+}
---
+++ Assets/Scripts/ChunkSpawner.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChunkSpawner : MonoBehaviour {
+
+
+    private System.Random random;
+    private float cooldown = 1.0f;
+    private float timer;
+
+    // Use this for initialization
+    void Start () {
+        random = new System.Random();
+        timer = 0.0f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(timer>0.0f)
+        {
+            timer -= Time.deltaTime;
+        }
+	
+	}
+    public void SpawnChunk()
+    {
+        GameObject go = (GameObject)Instantiate(Resources.Load("Chunk"+ random.Next(1,4)), transform.position, transform.rotation);
+        go.transform.parent = transform;
+    }
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag=="Chunk")
+        {
+            SpawnChunk();
+            timer = cooldown;
+        }
+    }
+}
