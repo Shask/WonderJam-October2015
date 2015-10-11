382d1e97e7fc358537afc51508ea51c74e1b3516
Steven Fougeron <steven.fougeron@gmail.com>
2015-10-11 16:36:15 +0200

    Pixel now have colors

--- Assets/Scripts/EnemyID.cs
+++ Assets/Scripts/EnemyID.cs
@@ -5,13 +5,16 @@ public class EnemyID : MonoBehaviour {
 
     public string input;
     private System.Random random;
+    public Material[] mat;
 
     // Use this for initialization
     void Start () {
         random = new System.Random();
-
+        int EnemyType = random.Next(1, 5);
         //generate a random input to kill it
-        input = "Fire1";//+ random.Next(0,4);
+        input = "Fire"+ EnemyType;//+ random.Next(0,4);
+        GetComponent<Renderer>().material = mat[EnemyType-1];
+
 	
 	}
 	
